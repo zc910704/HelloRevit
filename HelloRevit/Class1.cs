088a3cee87e50065b2d545c4426f359fb2ff754a
zc910704 <329188614@qq.com>
2018-09-20 19:42:07 +0800

    Hello Revit by zhangchen

---
+++ HelloRevit/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace HelloWorld
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Demo", "Hello World!");
+            return Result.Succeeded;
+        }
+    }
+}
